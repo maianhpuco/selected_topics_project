3cc2bbd55df22763bb1e77f211b86768289cafd9
Mai Anh Vu <maianhuel@gmail.com>
2024-11-30 13:27:45 -0600

    update

--- src/simclr/simclr.py
+++ src/simclr/simclr.py
@@ -95,66 +95,66 @@ class NTXentLoss(torch.nn.Module):
 
         return loss / (2 * self.batch_size) 
 
-class ResNetSimCLR(nn.Module):
-
-    def __init__(self, base_model, out_dim):
-        super(ResNetSimCLR, self).__init__()
-
-        # Use timm for model initialization
-        self.model = timm.create_model(base_model, pretrained=True, num_classes=0)  # num_classes=0 because we don't want final classification layer
-        print(f"Feature extractor: {base_model}")
-
-        # Get the number of features from the last fully connected layer
-        num_ftrs = self.model.fc.in_features  # Adjust this line depending on the model's final layer
-
-        # Feature extraction layers
-        self.features = nn.Sequential(*list(self.model.children())[:-1])  # Remove final classification layer
-
-        # Projection MLP (fully connected layers)
-        self.l1 = nn.Linear(num_ftrs, num_ftrs)
-        self.l2 = nn.Linear(num_ftrs, out_dim)
-
-    def forward(self, x):
-        h = self.features(x)  # Extract features
-        h = h.squeeze()  # Remove unnecessary dimensions
-
-        x = self.l1(h)  # First MLP layer
-        x = F.relu(x)  # Apply ReLU activation
-        x = self.l2(x)  # Final MLP layer (project to `out_dim`)
-        return h, x  # Return both the features and the projections 
-     
 # class ResNetSimCLR(nn.Module):
 
 #     def __init__(self, base_model, out_dim):
 #         super(ResNetSimCLR, self).__init__()
-#         self.resnet_dict = {"resnet18": models.resnet18(pretrained=False, norm_layer=nn.InstanceNorm2d),
-#                             "resnet50": models.resnet50(pretrained=False, norm_layer=nn.InstanceNorm2d)}
 
-#         resnet = self._get_basemodel(base_model)
-#         num_ftrs = resnet.fc.in_features
+#         # Use timm for model initialization
+#         self.model = timm.create_model(base_model, pretrained=True, num_classes=0)  # num_classes=0 because we don't want final classification layer
+#         print(f"Feature extractor: {base_model}")
 
-#         self.features = nn.Sequential(*list(resnet.children())[:-1])
+#         # Get the number of features from the last fully connected layer
+#         num_ftrs = self.model.fc.in_features  # Adjust this line depending on the model's final layer
 
-#         # projection MLP
+#         # Feature extraction layers
+#         self.features = nn.Sequential(*list(self.model.children())[:-1])  # Remove final classification layer
+
+#         # Projection MLP (fully connected layers)
 #         self.l1 = nn.Linear(num_ftrs, num_ftrs)
 #         self.l2 = nn.Linear(num_ftrs, out_dim)
 
-#     def _get_basemodel(self, model_name):
-#         try:
-#             model = self.resnet_dict[model_name]
-#             print("Feature extractor:", model_name)
-#             return model
-#         except:
-#             raise ("Invalid model name. Check the config file and pass one of: resnet18 or resnet50")
-
 #     def forward(self, x):
-#         h = self.features(x)
-#         h = h.squeeze()
+#         h = self.features(x)  # Extract features
+#         h = h.squeeze()  # Remove unnecessary dimensions
 
-#         x = self.l1(h)
-#         x = F.relu(x)
-#         x = self.l2(x)
-#         return h, x 
+#         x = self.l1(h)  # First MLP layer
+#         x = F.relu(x)  # Apply ReLU activation
+#         x = self.l2(x)  # Final MLP layer (project to `out_dim`)
+#         return h, x  # Return both the features and the projections 
+     
+class ResNetSimCLR(nn.Module):
+
+    def __init__(self, base_model, out_dim):
+        super(ResNetSimCLR, self).__init__()
+        self.resnet_dict = {"resnet18": models.resnet18(pretrained=True, norm_layer=nn.InstanceNorm2d),
+                            "resnet50": models.resnet50(pretrained=True, norm_layer=nn.InstanceNorm2d)}
+
+        resnet = self._get_basemodel(base_model)
+        num_ftrs = resnet.fc.in_features
+
+        self.features = nn.Sequential(*list(resnet.children())[:-1])
+
+        # projection MLP
+        self.l1 = nn.Linear(num_ftrs, num_ftrs)
+        self.l2 = nn.Linear(num_ftrs, out_dim)
+
+    def _get_basemodel(self, model_name):
+        try:
+            model = self.resnet_dict[model_name]
+            print("Feature extractor:", model_name)
+            return model
+        except:
+            raise ("Invalid model name. Check the config file and pass one of: resnet18 or resnet50")
+
+    def forward(self, x):
+        h = self.features(x)
+        h = h.squeeze()
+
+        x = self.l1(h)
+        x = F.relu(x)
+        x = self.l2(x)
+        return h, x 
     
 
 class SimCLR(object):
